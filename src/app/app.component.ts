3ffce75ef285835559bdf84ca11fdc5dcfc532ab
prathyusha chamarthy <prathyucp.2021@gmail.com>
2022-11-07 13:49:09 -0500

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDoItem } from './model/to-do-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toDoList';
+  
+  myToDoList:ToDoItem[]=[
+    {
+      task:"Fold Clothes",
+      completed:false,
+      delete:false,
+      index:1,
+      editable:false
+    },
+    {
+      task:"Prepare Dinner",
+      completed:true,
+      delete:false,
+      index:2,
+      editable:false
+    },
+    {
+      task:"Pick up Kids",
+      completed:false,
+      delete:false,
+      index:3,
+      editable:false
+    }
+  ];
+
+  filterDataByTag(searchTerm:string) {
+    searchTerm = "fold";
+    let newTodos:ToDoItem[] =[{ }]as ToDoItem[];
+    // filter the data array, based on some condition
+    this.myToDoList =  this.myToDoList.filter(item => {
+ 
+       return item.task.includes(searchTerm);
+    });
+    searchTerm = searchTerm.toLowerCase();
+    this.myToDoList = 
+    this.myToDoList = newTodos;
+    
+  }
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './component/todo-list/todo-list.component';
+import { TodoItemComponent } from './component/todo-item/todo-item.component';
+import { TodoFormComponent } from './component/todo-form/todo-form.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
